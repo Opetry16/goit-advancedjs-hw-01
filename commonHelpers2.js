77a1332beea1ab310872ad835c514f82e636cb75
Opetry16 <143953227+Opetry16@users.noreply.github.com>
2024-06-02 20:47:13 +0000

    Deploying to gh-pages from @ Opetry16/goit-advancedjs-hw-01@db2c05cf7c61f57a53d9b4bf8ac2b52ed6e7f486 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import{P as n,t as a}from"./assets/vendor-1e54b975.js";const e="videoplayer-current-time",c=document.querySelector("iframe"),t=new n(c),m=function(o){const r=o.seconds;localStorage.setItem(e,r)};t.on("timeupdate",a(m,1e3));t.setCurrentTime(localStorage.getItem(e)||0);
+//# sourceMappingURL=commonHelpers2.js.map
